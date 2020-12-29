a228522cf2a2cab13c28e993f10a11daffe5d175
pbator <prbator@gmail.com>
2020-12-29 22:49:54 +0100

    trees shadows shader

--- reverse_engineering/hlsl/PixelShader/EC4B6A44.hlsl
+++ reverse_engineering/hlsl/PixelShader/EC4B6A44.hlsl
@@ -1,6 +1,14 @@
-sampler samp;
+//tress shadows! trzeba ruszyc eran
+
+/*
+    ps_1_1
+    tex t0
+    mul r0, t0, c0
+	*/
+
+sampler samp0: register(s0);
 float4 p0 : register(c0);
 
-float4 main( float2 t0 : TEXCOORD0) : COLOR0 {
-	return tex2D(samp, t0) * p0;
+float4 main( float2 t0 : TEXCOORD0) : COLOR {
+	return tex2D(samp0, t0) * p0;
 }

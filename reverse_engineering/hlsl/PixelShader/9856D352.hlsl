802e285a873e682cd96b5d9b45e50f20462e06d7
pbator <prbator@gmail.com>
2020-12-29 22:06:18 +0100

    terrain shaders

--- reverse_engineering/hlsl/PixelShader/9856D352.hlsl
+++ reverse_engineering/hlsl/PixelShader/9856D352.hlsl
@@ -1,3 +1,19 @@
+//Terrain
+/*  
+    ps_1_1
+    tex t0
+    tex t1
+    texcoord t2
+    mul_x4_sat r0.xyz, v0, t0
+  + add_x4_sat r0.w, t1, -t2.z
+    mul_x4_sat r1.xyz, v1, t0
+  + add r0.w, c4, r0
+    lrp r1.xyz, t1, r1, r0
+    cnd r0.xyz, r0.w, r0, r1
+
+#line 11
+  + mul r0.w, t0.w, v0.w */
+
 sampler samp0: register(s0);
 sampler samp1: register(s1);
 float4 p4 : register(c4);
@@ -30,7 +46,7 @@ float4 main( float2 tex0 : TEXCOORD0, //tekstura podstawowa
 	
 	 // Calculate the final color using the fog effect equation.
 	float4 fogColor = {0.607843f, 0.917647f, 0.941176f, 1.0f};
-	result.xyz = fogFactor * result.xyz + (1.0 - fogFactor) * fogColor;
+	result.xyz = fogFactor * result.xyz + (1.0 - fogFactor);
 
 	
 	//result.xyz = green;
--- reverse_engineering/hlsl/VertexShader/22442EA8.hlsl
+++ reverse_engineering/hlsl/VertexShader/22442EA8.hlsl
@@ -1,3 +1,46 @@
+//terrain
+/*
+vs_3_0
+    dcl_position v0
+    dcl_normal v1
+    dcl_normal1 v2
+    dcl_texcoord v3
+    dcl_tangent v4
+    dcl_tangent1 v5
+    dcl_position o0
+    dcl_fog o1.x
+    dcl_texcoord o2.xy
+    dcl_texcoord1 o3.xy
+    dcl_texcoord2 o4
+    dcl_color o5
+    dcl_color1 o6
+    dp4 o0.x, v0, c10
+    dp4 o0.y, v0, c11
+    dp4 o0.z, v0, c12
+    dp4 r0.y, v0, c25
+    dp4 r0.x, v0, c26
+    mad o3.xy, r0, c27, c27.zwzw
+    mul r0.x, c30.y, v4.w
+    mad r0.x, v5.w, c30.x, r0.x
+    mul r0.yzw, r0.x, c29.xxyz
+    mul o5.xyz, r0.yzww, v4
+    mul o6.xyz, r0.yzww, v5
+    mul o5.w, c29.w, v1.w
+    dp4 o4, v0, c23
+    mad r0.y, c19.z, v0.z, c19.w
+    dp4 r0.z, v0, c13
+    mad r0.w, c19.x, r0.z, c19.y
+    mov o0.w, r0.z
+    min r0.y, r0.y, r0.w
+    max r0.y, r0.y, c0.x
+    add r0.y, r0.y, c1.x
+    add o1.x, -r0.x, r0.y
+    mov r0.xy, c38
+    mad r0.xy, v3.x, r0, c39
+    mad o2.xy, v3.y, c38.zwzw, r0
+    mov o6.w, v2.w
+*/
+
 //sampler samp0: register(s0);
 //sampler samp1: register(s1);
 
@@ -56,7 +99,7 @@ struct VS_OUTPUT
 	//Light tangent
 	float2 oT1: TEXCOORD1; // s(t1).w -> mapa cieni
 	//View tangent
-	float4 oT2: TEXCOORD2; // z -> wysokoœæ (Y)
+	float4 oT2: TEXCOORD2; // z -> wysokoï¿½ï¿½ (Y)
 	
 	float4 vDiffuse : COLOR0; // xyz diff lifght, w mieszanie tekstur
 	//Fog of war, alpha
@@ -99,6 +142,6 @@ VS_OUTPUT  main(VS_INPUT input) {
 	fog = fog + p1.x;
 	output.fog = fog - fogOfWar; //0 = max fog
 	
-	output.oT0 = input.vTex0.y * p38.zw + (input.vTex0.x * p38.xy + p39.xy);//Tekstura podstawowa
+	output.oT0 = mad(input.vTex0.y, p38.zw , mad (input.vTex0.x , p38.xy , p39.xy));//Tekstura podstawowa
 	return output;
 }
